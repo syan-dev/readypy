d0df1f9db37456fde529ef6f1081866c13837cf5
Nguyen Sy An <syan.vn@gmail.com>
2023-06-25 12:32:05 +0700

    add color to css light/dark

--- readypy/src/components/Header/Header.js
+++ readypy/src/components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     return (
         <div className={classes.header}>
             <h1>
-                <span className={classes.logo}><LogoIcon size={15} color="darkcyan" /></span>
+                <span className={classes.logo}><LogoIcon size={15} /></span>
                 <span className={classes.title}>ReadyPy</span>
             </h1>
             <div className={classes.actions}>
@@ -25,7 +25,7 @@ const Header = (props) => {
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                 <path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z" />
                             </svg>
-                            Mode
+                            Theme
                         </button>
                     </div>
                     <div className={classes.actionItem}>
--- readypy/src/components/Icon/Logo.js
+++ readypy/src/components/Icon/Logo.js
@@ -1,34 +1,36 @@
 import React from 'react';
+import classes from './Logo.module.css';
+
+
 
 const LogoIcon = (props) => {
     const size = props.size;
-    const color = props.color;
     const space = props.size/10;
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width={size*3 + space*2} height={size*2 + space}>
+        <svg xmlns="http://www.w3.org/2000/svg" width={size*3 + space*2} height={size*2 + space} className={classes.logo}>
             <rect 
                 x={size + space} y="0" 
                 width={size} height={size} 
-                fill={color} 
+                // fill={color} 
             />
             <rect 
                 x={(size) * 2 + space * 2} y="0" 
                 width={size} height={size} 
-                fill={color} 
+                // fill={color} 
             />
             <rect 
                 x="0"
                 y={size + space}
                 width={size} 
                 height={size} 
-                fill={color} 
+                // fill={color} 
             />
             <rect 
                 x={size + space} 
                 y={size + space} 
                 width={size} 
                 height={size} 
-                fill={color} 
+                // fill={color} 
             />
         </svg>
     );
